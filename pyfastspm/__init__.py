0f30c1f4a8598d00fadb0e9fe1cf0699b5f59e0e
matkrin <matkrin@protonmail.com>
2024-11-20 08:35:12 +0100

    Rename `conv_mat.py` to `filters.py` in `pyfastspm.artefact_removal` and update imports in toplevel `__init__.py`

--- pyfastspm/__init__.py
+++ pyfastspm/__init__.py
@@ -3,7 +3,7 @@ import logging
 from pyfastspm._version import version as __version__
 from pyfastspm._version import version_tuple as __version_tuple__
 
-from .artefact_removal.conv_mat import conv_mat
+from .artefact_removal.filters import conv_mat, mean_2d, median_2d, gaussian_2d
 from .artefact_removal.creep import Creep
 from .artefact_removal.drift import Drift
 from .artefact_removal.fft import (
