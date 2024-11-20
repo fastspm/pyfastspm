50d00ab85bb55fac0dfec980dbf5c147ce26a7d8
matkrin <matkrin@protonmail.com>
2024-11-20 08:30:59 +0100

    Additional 2D Filters (from most recent notebook)

--- pyfastspm/artefact_removal/conv_mat.py
+++ pyfastspm/artefact_removal/conv_mat.py
@@ -1,6 +1,8 @@
 import logging
 
 import numpy as np
+from numpy._typing import ArrayLike
+from scipy.ndimage import gaussian_filter, median_filter
 from scipy.signal import convolve2d
 from tqdm import tqdm
 
@@ -9,17 +11,20 @@ from ..fast_movie import FastMovie
 log = logging.getLogger(__name__)
 
 
-def conv_mat(fast_movie: FastMovie, matrix, image_range=None):
-    """Applies a convolutional filter defined by a matrix to a FastMovie.
+def conv_mat(
+    fast_movie: FastMovie,
+    matrix: ArrayLike,
+    image_range: tuple[int, int] | None = None,
+) -> None:
+    """Applies a convolutional filter defined by a matrix to a `FastMovie`.
 
     Args:
-        fast_movie: FastMovie object
-        matrix: filter matrix
-        image_range: range of images to be interpolated
+        fast_movie: `FastMovie` object
+        matrix: Filter kernel
+        image_range: Range of images to be interpolated
 
     Returns:
-        nothing
-
+        None: Modifies `FastMovie.data` in-place
     """
     if fast_movie.mode != "movie":
         raise ValueError("you must first reshape your data in movie mode.")
@@ -51,3 +56,50 @@ def conv_mat(fast_movie: FastMovie, matrix, image_range=None):
             fast_movie.data[frame] = convolve2d(
                 fast_movie.data[frame], matrix, boundary="symm"
             )[leny:-leny, lenx:-lenx]
+
+
+def mean_2d(fast_movie: FastMovie, pixel_width: int) -> None:
+    """Applies a 2D mean filter of size (pixel_width, pixel_width) to each
+        frame of a `FastMovie`
+
+    Args:
+        fast_movie: A `FastMovie` instance
+        pixel_width: Size of the kernel
+
+    Returns:
+        None: Modifies `FastMovie.data` in-place
+    """
+    kernel_shape = (pixel_width, pixel_width)
+    kernel = np.ones(kernel_shape) / (pixel_width * pixel_width)
+    for frame in fast_movie.data:
+        frame = convolve2d(frame, kernel, mode="same")
+
+
+def median_2d(fast_movie: FastMovie, pixel_width: int) -> None:
+    """Applies a 2D median filter of size (pixel_width, pixel_width) to each
+        frame of a `FastMovie`
+
+    Args:
+        fast_movie: A `FastMovie` instance
+        pixel_width: Size of the kernel
+
+    Returns:
+        None: Modifies `FastMovie.data` in-place
+    """
+    size = (pixel_width, pixel_width)
+    for frame in fast_movie.data:
+        frame = median_filter(frame, size=size)
+
+
+def gaussian_2d(fast_movie: FastMovie, pixel_width: int) -> None:
+    """Applies a 2D gaussian filter to each frame of a `FastMovie`
+
+    Args:
+        fast_movie: A `FastMovie` object
+        pixel_width: Proportional to the size of the gaussian kernel
+
+    Returns:
+        None: Modifies `FastMovie.data` in-place
+    """
+    for frame in fast_movie.data:
+        frame = gaussian_filter(frame, pixel_width - 1, truncate=0.5)
